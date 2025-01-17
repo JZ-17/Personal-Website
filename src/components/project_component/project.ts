de8a1b65b4abe2c95d809a03fef2f185cc7e2d00
Joshua Zhang <josh@joshuas-air-5.lan>
2025-01-16 22:17:50 -0500

    Fix projects section, change close button, fix margins for iPhone, and dab <o/

--- src/components/project_component/project.ts
+++ src/components/project_component/project.ts
@@ -4,9 +4,9 @@ export const project = [
       company: "HackUTD",
       award: "1st Place in Pinata Platinum Sponsor Category",
       accomplishment:
-        "Developed and deployed an AI-powered app that uses photos of your food to recognize ingredients, determine portion sizes, and calculate macro breakdowns.",
+        "Developed and deployed an AI-powered app that uses photos of your food to recognize ingredients, determine portion sizes, and calculate macro breakdowns",
       actions: [
-        "Designed and deployed a TensorFlow-based neural network leveraging the Food101 dataset to classify food items and identify ingredients, integrating USDA data to accurately predict macro-nutrient distribution.",
+        "Designed and deployed a TensorFlow-based neural network leveraging the Food101 dataset to classify food items and identify ingredients, integrating USDA data to accurately predict macro-nutrient distribution",
         "Developed a front-end user interface enabling users to scan or upload meal photos and analyze progress through goal-oriented graphs.",
       ],
       skills: [ "React","Typescript","Next JS","Tailwind CSS","ShadCN","Node JS","Pinata Cloud","TensorFlow",],
@@ -20,10 +20,10 @@ export const project = [
       company: "Harvard Data Science Initiative Hackathon",
       award: null,
       accomplishment:
-        "Analyzed USDA/NASS data to identify statistically significant trends between average farmer age and agricultural productivity from 1997 to 2022.",
+        "Analyzed USDA/NASS data to identify statistically significant trends between average farmer age and agricultural productivity from 1997 to 2022",
       actions: [
-        "Explored and analyzed USDA/NASS data from the 2022 Agricultural Census, examining the correlation between average farmer age and agricultural productivity, measured by inflation-adjusted 2022 dollar values and raw sales.",
-        "Applied linear regressions in Python and visualized findings with choropleth maps, identifying a statistically significant negative trend between aging farmers and productivity across U.S. counties, with the strongest effects in southern regions (p-value < 0.01).",
+        "Explored and analyzed USDA/NASS data from the 2022 Agricultural Census, examining the correlation between average farmer age and agricultural productivity, measured by inflation-adjusted 2022 dollar values and raw sales",
+        "Applied linear regressions in Python and visualized findings with choropleth maps, identifying a statistically significant negative trend between aging farmers and productivity across U.S. counties, with the strongest effects in southern regions (p-value < 0.01)",
       ],
       skills: ["Python", "Pandas", "NumPy", "Matplotlib", "Seaborn", "Statsmodels"],
       link: null,
@@ -36,11 +36,11 @@ export const project = [
       company: "Personal Project",
       award: null,
       accomplishment:
-        "Developed a random forest algorithm that predicts GHG emissions of buildings in Cambridge, MA with an OOB R-Squared of 0.70 and RMSE of 0.373.",
+        "Developed a random forest algorithm that predicts GHG emissions of buildings in Cambridge, MA with an OOB R-Squared of 0.70 and RMSE of 0.373",
       actions: [
-        "Utilized the 2022 Cambridge BEUDO dataset to develop a random forest algorithm that predicts GHG emissions of buildings in Cambridge, MA with an OOB R-Squared value of 0.70, RMSE of 0.373, and Train R-Squared of 0.861.",
-        "Used OOB error to determine the optimal number of trees and max depth of the random forest to maximize generalization against unseen data.",
-        "Utilized Lasso and Ridge Regressions to determine predictor significance.",
+        "Utilized the 2022 Cambridge BEUDO dataset to develop a random forest algorithm that predicts GHG emissions of buildings in Cambridge, MA with an OOB R-Squared value of 0.70, RMSE of 0.373, and Train R-Squared of 0.861",
+        "Used OOB error to determine the optimal number of trees and max depth of the random forest to maximize generalization against unseen data",
+        "Utilized Lasso and Ridge Regressions to determine predictor significance",
       ],
       skills: ["Scikit-Learn", "Matplotlib", "Pandas", "NumPy"],
       link: null,
@@ -53,11 +53,11 @@ export const project = [
       company: "Personal Project",
       award: null,
       accomplishment:
-        "Developed fixed-effects regression models to predict bank profitability (Adj. R-Squared: 0.987) and stability (Adj. R-Squared: 0.110), with interpretations of results.",
+        "Developed fixed-effects regression models to predict bank profitability (Adj. R-Squared: 0.987) and stability (Adj. R-Squared: 0.110), with interpretations of results",
       actions: [
-        "Processed FFIEC Call Report data by handling mergers, filtering for banks with assets exceeding $1 billion, and identifying target variables for profitability and stability predictions.",
-        "Implemented Ridge and Lasso regressions to optimize variable selection and improve model accuracy.",
-        "Developed fixed-effects regression models to account for time-specific and bank-specific factors impacting profitability and stability.",
+        "Processed FFIEC Call Report data by handling mergers, filtering for banks with assets exceeding $1 billion, and identifying target variables for profitability and stability predictions",
+        "Implemented Ridge and Lasso regressions to optimize variable selection and improve model accuracy",
+        "Developed fixed-effects regression models to account for time-specific and bank-specific factors impacting profitability and stability",
       ],
       skills: ["R", "ggplot2", "dplyr", "tidyverse", "olsrr"],
       link: null,
@@ -69,10 +69,10 @@ export const project = [
       title: "Ramen Rampage",
       company: "Personal Project",
       award: null,
-      accomplishment: "Created a incremental-style game utilzing Swift and Xcode and custom-made graphics.",
+      accomplishment: "Created a incremental-style game utilizing Swift and Xcode and custom-made graphics",
       actions: [
-        "Utilized Swift and Xcode to develop an incremental-style game where players click to make ramen and upgrade their ramen shop in hopes of becoming the world's greatest ramen maker :)",
-        "Created custom pixel art and animations for the game.",
+        "Utilized Swift and Xcode to develop an incremental-style game where players click to make ramen and upgrade their ramen shop in hopes of becoming the fastest Ramen producer in the world :)",
+        "Created custom pixel art and animations for the game",
       ],
       skills: ["Swift", "Xcode"],
       link: null,
@@ -81,13 +81,14 @@ export const project = [
       priority: false,
     },
     {
-      title: "This website :)",
+      title: "This Website :)",
       company: "Personal Project",
       award: null,
-      accomplishment: "Created a personal website using Next.js and React.",
+      accomplishment: "Created a personal website using Next.js and React to showcase my experiences, projects, hobbies, and contact information in a complete manner",
       actions: [
         "Used Next.js and React to develop a personal website showcasing my experiences, projects, hobbies, and contact information.",
-        "Implemented Tailwind CSS for styling and Framer Motion for animations.",
+        "Implemented Tailwind CSS for styling and mobile compatibility",
+        "Utilized Framer Motion for animations and transitions",
       ],
       skills: ["Next JS", "React", "Typescript", "Javascript", "Tailwind CSS", "Framer Motion"],
       link: "https://joshzhang.tech",
@@ -100,10 +101,10 @@ export const project = [
       company: "Virginia State Science and Engineering Fair",
       award: "1st Place in the Environmental Engineering Category",
       accomplishment:
-        "Created the world's first applicable sound-based fire extinguisher using low bass frequencies to extinguish class A-D fires in a 5-foot radius, increasing efficiency by 400%.",
+        "Created the world's first applicable sound-based fire extinguisher using low bass frequencies to extinguish class A-D fires in a 5-foot radius, increasing efficiency by 400%",
       actions: [
-        "Tested 108 combinations of the extinguisher by manipulating variables of wave type, aperture size, and base frequency range (20-60 Hz) for maximum distance.",
-        "Used the top 12 combinations to test against class A-C fire types to find the best extinguisher configuration.",
+        "Tested 108 combinations of the extinguisher by manipulating variables of wave type, aperture size, and base frequency range (20-60 Hz) for maximum distance",
+        "Used the top 12 combinations to test against class A-C fire types to find the best extinguisher configuration",
       ],
       skills: ["Research", "Electrical Engineering", "Acoustics"],
       link: "https://www.youtube.com/watch?v=E5WvfYGh_sM",
