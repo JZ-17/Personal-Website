2bc33cb1f41994584865a187ac2554265cf427ac
Joshua Zhang <josh@dhcp-10-250-225-178.harvard.edu>
2025-02-18 22:31:44 -0500

    Added load in animation with framer motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
-"use client"
+"use client";
+
+import { motion } from "framer-motion";
 import Navbar from "@/components/navbar_component/navbar";
 import Intro from "@/components/intro_component/Intro";
 import Me from "@/components/me_component/me";
@@ -9,8 +11,6 @@ import { project as projectData } from "@/components/project_component/project";
 import { useEffect } from "react";
 import "@/app/globals.css";
 import Footer from "@/components/footer_component/footer";
-
-
 
 export default function Home() {
   useEffect(() => {
@@ -28,23 +28,45 @@ export default function Home() {
     }
   }, []);
 
+  const fadeInUp = {
+    hidden: { opacity: 0, y: 0 },
+    visible: { opacity: 1, y: 20, transition: { duration: 2.5 } },
+  };
+
   return (
     <>
-        {/* Background Stars */}
-        <div id="stars-container"></div>
+      {/* Background Stars */}
+      <motion.div
+        id="stars-container"
+        initial="hidden"
+        animate="visible"
+        variants={fadeInUp}
+      ></motion.div>
 
-        {/* Main Content */}
-        <main className="flex min-h-screen flex-col">
-          <div className="container mt-24 mx-auto px-12 py-4">
-            <Navbar />
+      {/* Main Content */}
+      <main className="flex min-h-screen flex-col">
+        <div className="container mt-24 mx-auto px-12 py-4">
+          <Navbar />
+          <motion.div initial="hidden" animate="visible" variants={fadeInUp}>
             <Intro />
+          </motion.div>
+          <motion.div initial="hidden" animate="visible" variants={fadeInUp}>
             <Me />
+          </motion.div>
+          <motion.div initial="hidden" animate="visible" variants={fadeInUp}>
             <Experience />
-            <Projects project = { projectData }/>
+          </motion.div>
+          <motion.div initial="hidden" animate="visible" variants={fadeInUp}>
+            <Projects project={projectData} />
+          </motion.div>
+          <motion.div initial="hidden" animate="visible" variants={fadeInUp}>
             <Email />
+          </motion.div>
+          <motion.div initial="hidden" animate="visible" variants={fadeInUp}>
             <Footer />
-          </div>
-        </main>
+          </motion.div>
+        </div>
+      </main>
     </>
   );
 }
