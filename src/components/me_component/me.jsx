13cc537fde696d26df5ba428dc0e2dd4f503f2a0
Joshua Zhang <josh@Joshuas-MacBook-Air-63.local>
2025-06-13 15:55:58 -0400

    added comma to me lol

--- src/components/me_component/me.jsx
+++ src/components/me_component/me.jsx
@@ -11,7 +11,7 @@ const tab_data = [
     content: (
       <ul className="list-disc tab-list">
         <li>Programming Languages: Python, R, JavaScript, C++, Swift, SQL</li>
-        <li>Machine Learning Frameworks: Scikit-Learn, TensorFlow, Keras PyTorch</li>
+        <li>Machine Learning Frameworks: Scikit-Learn, TensorFlow, Keras, PyTorch</li>
         <li>Front-End Development: React, Redux, Firebase, Tailwind, CSS</li>
         <li>Data Analysis: Pandas, Matplotlib, Seaborn, StatsModels</li>
         <li>Other Tools: Hugging Face, AWS Cloud Services, Supabase, OpenAI API, Blender, Fusion360, Photoshop, Premiere Pro</li>
