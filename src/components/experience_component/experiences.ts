848d5b4770662e5604ff22cd3f3f42da2b2d17f9
Joshua Zhang <josh@Joshuas-MacBook-Air-63.local>
2025-11-12 17:22:28 -0500

    Update Resume and Scale XP

--- src/components/experience_component/experiences.ts
+++ src/components/experience_component/experiences.ts
@@ -12,6 +12,7 @@ export const experiences = [
     company: "Scale AI",
     description: [
       "Fine-tune and evaluate large-scale generative AI models for enterprise applications, enhancing multi-step reasoning, problem decomposition, and code synthesis",
+      "Create controlled LLM failure frameworks, optimize and debug model-generated code, and develop evaluation rubrics to benchmark reasoning and code accuracy",
     ],
     link: "https://scale.com/",
   },
